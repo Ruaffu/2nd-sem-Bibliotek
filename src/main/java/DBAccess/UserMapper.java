2d8a3ae62bac1c8c10c616dbe048984e938a9437
Nikolaj B Hemmeshøj <nbh@cphbusiness.dk>
2021-03-16 17:08:04 +0100

    er igang med kundeklassen

--- src/main/java/DBAccess/UserMapper.java
+++ src/main/java/DBAccess/UserMapper.java
@@ -35,57 +35,57 @@ public class UserMapper {
 
     }
 
-    public static List<Kunde> getKunder() {
-
-        List<Kunde> kundeList = new ArrayList<>();
-
-        String sql = "select * from Kunde ";
-
-        try (Connection con = ConnectionConfiguration.getConnection();
-             PreparedStatement ps = con.prepareStatement(sql))  {
-            ResultSet resultSet = ps.executeQuery();
-            while (resultSet.next()){
-                int id = resultSet.getInt("idKunde");
-                String navn = resultSet.getString("Kundenavn");
-
-                Kunde kunde = new Kunde(id, navn);
-               kundeList.add(kunde);
-            }
-        } catch (SQLException e) {
-            System.out.println("Fejl i connection til database");
-            e.printStackTrace();
-        }
-        return kundeList;
-    }
-
-
-    public static String deleteKunde(String kundeNavn) {
-
-
-        try {
-//            Connection con = Connector.connection();
-            Connection con = ConnectionConfiguration.getConnection();
-            String SQL = "delete from Kunde where Kundenavn = ?";
-            PreparedStatement ps = con.prepareStatement(SQL);
-            ps.setString(1, kundeNavn);
-
-            int rew = ps.executeUpdate();
-
-            if (rew > 0 ) {
-
-                return kundeNavn + " " + " er nu blevet slettet";
-            }
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-
-
-            return kundeNavn + " " + "fandtes ikke i listen";
-
-
-
-    }
+//    public static List<Kunde> getKunder() {
+//
+//        List<Kunde> kundeList = new ArrayList<>();
+//
+//        String sql = "select * from Kunde ";
+//
+//        try (Connection con = ConnectionConfiguration.getConnection();
+//             PreparedStatement ps = con.prepareStatement(sql))  {
+//            ResultSet resultSet = ps.executeQuery();
+//            while (resultSet.next()){
+//                int id = resultSet.getInt("idKunde");
+//                String navn = resultSet.getString("Kundenavn");
+//
+//                Kunde kunde = new Kunde(id, navn);
+//               kundeList.add(kunde);
+//            }
+//        } catch (SQLException e) {
+//            System.out.println("Fejl i connection til database");
+//            e.printStackTrace();
+//        }
+//        return kundeList;
+//    }
+//
+//
+//    public static String deleteKunde(String kundeNavn) {
+//
+//
+//        try {
+////            Connection con = Connector.connection();
+//            Connection con = ConnectionConfiguration.getConnection();
+//            String SQL = "delete from Kunde where Kundenavn = ?";
+//            PreparedStatement ps = con.prepareStatement(SQL);
+//            ps.setString(1, kundeNavn);
+//
+//            int rew = ps.executeUpdate();
+//
+//            if (rew > 0 ) {
+//
+//                return kundeNavn + " " + " er nu blevet slettet";
+//            }
+//
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//
+//            return kundeNavn + " " + "fandtes ikke i listen";
+//
+//
+//
+//    }
 }
---
+++ src/main/java/FunctionLayer/Kunde.java
@@ -0,0 +1,47 @@
+package FunctionLayer;
+
+public class Kunde {
+
+    private int idKunde;
+    private String kundeNavn;
+    private String adresse;
+    private String postNr;
+
+    public Kunde(String kundeNavn, String adresse, String postNr) {
+        this.kundeNavn = kundeNavn;
+        this.adresse = adresse;
+        this.postNr = postNr;
+    }
+
+    public int getIdKunde() {
+        return idKunde;
+    }
+
+    public void setIdKunde(int idKunde) {
+        this.idKunde = idKunde;
+    }
+
+    public String getKundeNavn() {
+        return kundeNavn;
+    }
+
+    public void setKundeNavn(String kundeNavn) {
+        this.kundeNavn = kundeNavn;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getPostNr() {
+        return postNr;
+    }
+
+    public void setPostNr(String postNr) {
+        this.postNr = postNr;
+    }
+}
